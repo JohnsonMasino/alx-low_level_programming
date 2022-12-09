1579ec617631eba68603a3187c810bc7a3afca8e
Johnson Masino <johnsonmasino@gmail.com>
2022-12-08 19:50:32 -0800

    UNIX is basically a simple operating system, but you have to be a genius to understand the simplicity

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,6 +1,6 @@
 #include <sstdio.h>
 /**
- * main - print out sizes of data types in c
+ * main - print out sizes of data types in C
  * code by Johnson Masino
  * Return: 0
 */
@@ -12,10 +12,7 @@ int main(void)
 	long long int d;
 	float f;
 
-	printf("Size of a char: %lu byte(S)\n", (unsigned long)sizeof(a));
-	printf("Size of an int: %lu byte(S)\n", (unsigned long)sizeof(b));
+	printf("Size of a char: %lu byte(s)\n", (unsigned long)sizeof(a));
+	printf("Size of an int: %lu byte(s)\n", (unsigned long)sizeof(f));
 	printf("Size of a long int: %lu byte(S)\n", (unsigned long)sizeof(c));
-	printf("Size of a long long int: %lu byte(S)\n", (unsigned long)sizeof(d));
-	printf("Size of a float: %lu byte(S)\n", (unsigned long)sizeof(f));
-	return (0);
-}
+	printf("Size
