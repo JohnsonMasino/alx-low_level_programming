9f2a251a19134d2132da2dbc5e04c13d97638f86
Johnson Masino <johnsonmasino@gmail.com>
2023-01-09 07:05:05 -0800

    Neither irony nor sarcasm is argument

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - multiplies two numbers.
+ * @argc: argument count
+ * @argv: arrays of arguments
+ * Return: Always 0 (Success)
+ * Code by Masino
+ */
+
+int main(int argc, char **argv)
+{
+	int x, y;
+
+	if (argc < 3)
+	{
+		printf("Error\n");
+		return (1);
+	}
+
+	x = atoi(argv[1]);
+	y = atoi(argv[2]);
+	printf("%d\n", x * y);
+
+	return (0);
+}
