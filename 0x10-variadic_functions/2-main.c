2dccd08dec01fc9d9d2049a1e24c9cadcdddfc10
Johnson Masino <johnsonmasino@gmail.com>
2023-01-19 12:07:33 -0800

    One woman's constant is another woman's variable

---
+++ 0x10-variadic_functions/2-main.c
@@ -0,0 +1,12 @@
+#include "variadic_functions.h"
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+    print_strings(", ", 2, "Johnson", "Masino");
+    return (0);
+}
