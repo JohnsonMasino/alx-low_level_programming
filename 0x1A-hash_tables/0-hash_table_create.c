534f3ced8a21b9c07c2d98aa9c56dfb56a668479
JohnsonMasino <johnsonmasino@gmail.com>
2023-04-07 07:08:05 -0700

    >>> ht = {}

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -3,6 +3,7 @@
 /*
  * hash_table_create - creates a hash table
  * @size: size of the array
+ *
  * Return: pointer to the newly created hash table
  */
 hash_table_t *hash_table_create(unsigned long int size)
