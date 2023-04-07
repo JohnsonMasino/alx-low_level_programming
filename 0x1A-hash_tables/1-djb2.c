3b62d9c15531a9ca60cfa8182d4bda9b377ec701
JohnsonMasino <johnsonmasino@gmail.com>
2023-04-07 15:30:06 -0700

    Edited

--- 0x1A-hash_tables/1-djb2.c
+++ 0x1A-hash_tables/1-djb2.c
@@ -1,10 +1,10 @@
 #include "hash_tables.h"
 
 /**
- * hash_djb2 - implementation of the djb2 algorithm
- * @str: string used to generate hash value
+ * hash_djb2 - Implements the djb2 algorithm
+ * @str: The string, used to generate hash value
  *
- * Return: hash value
+ * Return: The calculated hash value
  */
 unsigned long int hash_djb2(const unsigned char *str)
 {
